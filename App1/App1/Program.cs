39c2b924f27db1868cf42a1635397992c30fdf1d
p1-21 <p1-21@stud.local>
2025-02-03 13:54:17 +0300

    Калькулятор с вводом а и б

---
+++ App1/App1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq.Expressions;
+
+class Program
+{
+    static double A, B;
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Ввод A");
+            Console.WriteLine("2. Ввод Б");
+            Console.WriteLine("3. Сложить");
+            Console.WriteLine("4. Вычесть");
+
+            string operations = Console.ReadLine();
+
+            switch (operations) {
+                case "1":
+                    {
+                        A = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    }
+                case "2": 
+                    {
+                        B = Convert.ToDouble(Console.ReadLine());
+                        break;
+                    }
+            }
+        }
+    }
+}
